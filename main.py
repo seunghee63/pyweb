3bb8d9e80e8f70b73c981592efbb461728bc9a65
Ubuntu <ubuntu@ip-172-31-17-110.ap-northeast-2.compute.internal>
2019-05-13 19:42:15 +0000

    complement library system

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pymongo
 mongo_url = 'mongodb+srv://song:yeji9140@cluster0-v0lp5.mongodb.net/test?retryWrites=true'
 client = pymongo.MongoClient(mongo_url)
 db = pymongo.database.Database(client,'Cluster0')
+books = pymongo.collection.Collection(db,'Books')
 
 app = Flask(__name__)
 
@@ -14,10 +15,11 @@ def score():
 #요청은 request.form에 다 담김!
 @app.route('/books', methods=['GET','POST'])
 def result():
-    if request.method == 'GET': #입력값 없이 바로 주소쳐서 넘어갔을 경우
-        return "It's GET"
-    
-    return render_template('books.html')  
+    if request.method == 'POST': #입력값 없이 바로 주소쳐서 넘어갔을 경우
+        books.insert_one(request.form.to_dict(flat='true'))
+      
+    #return books.watch()
+    return render_template('books.html',result = books.find({})) 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port = 5000)
