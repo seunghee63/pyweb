eefc986ed127670ef0f9e02ff628f755376a4e88
Ubuntu <ubuntu@ip-172-31-17-110.ap-northeast-2.compute.internal>
2019-05-16 10:53:50 +0000

    imcomplete _ indentation

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template, request, jsonify, session
+from flask import Flask, render_template, request, jsonify, session, json, redirect, url_for
 import pymongo
 
 mongo_url = 'mongodb+srv://song:yeji9140@cluster0-v0lp5.mongodb.net/test?retryWrites=true'
 client = pymongo.MongoClient(mongo_url)
 db = pymongo.database.Database(client,'Cluster0')
 books = pymongo.collection.Collection(db,'Books')
-users = pymongo.collection.Collection(db,'Users')
+Users = pymongo.collection.Collection(db,'Users')
 
 
 app = Flask(__name__)
+app.secret_key = "111"
+
+#with open("mongoDB,json") as Json:
+#    user_doc = json.loads(Json.read())
+
 
 @app.route('/register')
 def score():
@@ -26,11 +31,22 @@ def result():
 @app.route('/signup', methods=['GET','POST'])
 def signup():
     if request.method == 'GET':
-        return render_template('singip.html')
+        return render_template('signup.html')
     elif request.method == 'POST':
         Users.insert_one(request.form.to_dict(flat='true'))
         session['userEmail'] = request.form['userEmail']
-        return render_template('welcome.html', info = session['userEmail'])
+	return render_template('welcome.html',info = session['userEmail'])
+
+@app.route('/signin', methods=['GET''POST'])
+def signin():
+    if request.method == 'GET':
+        return render_template('signin.html')
+    elif request.method == 'POST':
+        if Users.find_one(request.form.to_dict(flat='true')) is not None:
+	    session['userEmail'] = request.form['userEmail']
+            return render_template('welcome.html', info = session['userEmail'])
+	return redirect(url_for('signin'))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port = 5000)
