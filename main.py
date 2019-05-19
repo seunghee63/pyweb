ec1109e19edb679d54554e7ec6acd1dd844877cd
seunghee63 <0603yang@naver.com>
2019-05-19 18:39:58 +0000

    modify mongo_url

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, request, jsonify, session, json, redirect, url_for
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 import pymongo
 from datetime import timedelta
+import json
 
-mongo_url = 'mongodb+srv://song:yeji9140@cluster0-v0lp5.mongodb.net/test?retryWrites=true'
+with open("mongoDB.json") as Json:
+    user_doc = json.loads(Json.read())
+
+mongo_url = 'mongodb+srv://'+ user_doc["MongoID"] + ':'+ user_doc['MongoPassword'] + user_doc["MongoURL"]
 client = pymongo.MongoClient(mongo_url)
 db = pymongo.database.Database(client, 'Cluster0')
 books = pymongo.collection.Collection(db, 'Books')
@@ -11,8 +15,6 @@ users = pymongo.collection.Collection(db, 'Users')
 app = Flask(__name__)
 app.secret_key = "111"
 
-# with open("mongoDB,json") as Json:
-#    user_doc = json.loads(Json.read())
 
 @app.route('/register')
 def register():
